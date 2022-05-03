3f9b9171baf29bb40b7f8a62a95e3d72981aae6c
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2022-05-03 12:23:57 +0800

    comment bootstrap

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Tooltip from '@mui/material/Tooltip';
 import "../../scss/Projects.scss";
 
